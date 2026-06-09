68286b7937f0e9ae624244ee95700955d2c09432
유수명 <sumung5@gmail.com>
2026-06-09 21:29:53 +0900

    생성 시에 empty activity로 프로젝트를 생성하여 없던 layout 폴더와 Activity_main.xml 파일 구현 및 Menu 폴더와 menu_bottom_nav.xml 생성 Gradle 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,15 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
     // 코루틴
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    // Google Maps
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
 }
